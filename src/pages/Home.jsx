a10c068ccff89029ab70d6c5387c92d009b6ffca
Macktireh <abdimack97@gmail.com>
2022-06-22 23:51:39 +0200

    Creation de la page d'acceuil ajout des élements principale

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,42 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-page">
+      <div className="container-img"></div>
+      <div className="container-content">
+        <div className="farme-herder">
+          <img
+            className="logo"
+            src="./static/svg/twitter.svg"
+            alt="logo Twitter"
+          />
+        </div>
+        <div className="frame-title">
+          <h1>Ça se passe maintenant</h1>
+          <h2>Rejoignez Twitter dès aujourd'hui.</h2>
+        </div>
+        <div className="frame-btn-connexion"></div>
+      </div>
+      <div className="container-footer">
+        <a id="items-footer-home">À propos</a>
+        <a id="items-footer-home">Centre d'assistance</a>
+        <a id="items-footer-home">Conditions d’utilisation</a>
+        <a id="items-footer-home">Politique de Confidentialité</a>
+        <a id="items-footer-home">Politique relative aux cookies</a>
+        <a id="items-footer-home">Accessibilité</a>
+        <a id="items-footer-home">Informations sur les publicités</a>
+        <a id="items-footer-home">Blog</a>
+        <a id="items-footer-home">Statut</a>
+        <a id="items-footer-home">Carrières</a>
+        <a id="items-footer-home">Ressources de la marque</a>
+        <a id="items-footer-home">Publicité</a>
+        <a id="items-footer-home">Marketing</a>
+        <a id="items-footer-home">Twitter pour les professionnels</a>
+        <a id="items-footer-home">Développeurs</a>
+        <a id="items-footer-home">Répertoire</a>
+        <a id="items-footer-home">Paramètres</a>
+        <a id="items-footer-home">© 2022 Twitter, Inc.</a>
+      </div>
     </div>
   );
 };
