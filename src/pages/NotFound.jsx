7a8c7f605ce091e34d18df11305c3039d24c769e
Macktireh <abdimack97@gmail.com>
2022-06-28 23:03:13 +0200

    ajout de page NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
+  useEffect(() => {
+    const container = document.getElementById("container-404");
+    window.onmousemove = (e) => {
+      let x = -e.clientX / 5;
+      let y = -e.clientY / 5;
+      container.style.backgroundPositionX = x + "px";
+      container.style.backgroundPositionY = y + "px";
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>NotFound Error 404</h1>
+    <div className="notfound">
+      <div id="container-404">
+        <div className="content">
+          <h2>404</h2>
+          <h4>Opps! page not found</h4>
+          <p>
+            La page que vous recherchez n'existe pas. Il se peut que vous ayez
+            mal saisi l'adresse ou que la page ait été déplacée.
+          </p>
+          <Link to="/">
+            <span>Revenir à la page d'acceuil</span>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
