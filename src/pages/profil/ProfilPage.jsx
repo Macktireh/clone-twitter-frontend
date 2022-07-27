bbc3c892220baecca332861893e33a6d25f3aff7
Macktireh <abdimack97@gmail.com>
2022-07-27 22:48:20 +0200

    add a profile page

---
+++ src/pages/profil/ProfilPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfilPage = () => {
+  return (
+    <div className="profil-page">
+      <h1>Welcome to Profil</h1>
+    </div>
+  );
+};
+
+export default ProfilPage;
