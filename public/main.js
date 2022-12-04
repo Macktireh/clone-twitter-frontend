087687daa71e77355ce6148a7a7187b782f76ed5
Macktireh <abdimack97@gmail.com>
2022-12-04 01:13:53 +0100

    Configuration for the desktop application with electron and for PWA.

---
+++ public/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      enableRemoteModule: true
+    }
+  })
+
+  win.loadURL('http://localhost:3000')
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function() {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
