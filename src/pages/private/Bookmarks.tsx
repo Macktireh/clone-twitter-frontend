7824abce985f70374a459fc01a54817565c290db
Macktireh <abdimack97@gmail.com>
2023-01-08 22:02:45 +0100

    display an image if the bookmarks list is empty

--- src/pages/private/Bookmarks.tsx
+++ src/pages/private/Bookmarks.tsx
@@ -9,20 +9,25 @@ import ListBookmarkTweet from "@/components/bookmark/ListCardBookmarkTweet";
 import getAllUsersAction from "@/actions/user/getAllUsers.action";
 import getAllPostAction from "@/actions/post/getAllPost.action";
 import { privateRoutes } from "@/routes/private.routes";
-import { IRootState, IPropsRootStateType } from "@/models";
+import { IRootState, IPropsRootStateType, TBookmarkReducerType } from "@/models";
+import getBookmarks from "@/actions/bookmark/getBookmarks.action";
 
 interface propsTypes
   extends Omit<IPropsRootStateType, "postsLikes" | "comments" | "followers" | "following" | "peopleConnect"> {
+  bookmarks: TBookmarkReducerType;
   getAllPostAction: () => Promise<void>;
   getAllUsersAction: () => Promise<void>;
+  getBookmarks: () => Promise<void>;
 }
 
 const Bookmarks: React.FC<propsTypes> = ({
   currentUser,
   users,
   posts,
+  bookmarks,
   getAllPostAction,
   getAllUsersAction,
+  getBookmarks,
 }) => {
   const flag = React.useRef(false);
 
@@ -31,10 +36,11 @@ const Bookmarks: React.FC<propsTypes> = ({
     if (!flag.current) {
       getAllPostAction();
       getAllUsersAction();
+      getBookmarks();
       flag.current = true;
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentUser, posts, users]);
+  }, [currentUser, posts, users, bookmarks]);
 
   return (
     <>
@@ -48,7 +54,7 @@ const Bookmarks: React.FC<propsTypes> = ({
               currentUser={currentUser}
             />
           </section>
-          {posts && posts.length > 0 ? (
+          {bookmarks && bookmarks.length > 0 ? (
             <ListBookmarkTweet currentUser={currentUser} users={users} posts={posts} />
           ) : (
             <div className="not-book">
@@ -74,8 +80,10 @@ const BookmarksConnectWithStore: React.FC<propsTypes> = ({
   currentUser,
   users,
   posts,
+  bookmarks,
   getAllPostAction,
   getAllUsersAction,
+  getBookmarks,
 }) => {
   return (
     <Layout>
@@ -83,8 +91,10 @@ const BookmarksConnectWithStore: React.FC<propsTypes> = ({
         currentUser={currentUser}
         users={users}
         posts={posts}
+        bookmarks={bookmarks}
         getAllPostAction={getAllPostAction}
         getAllUsersAction={getAllUsersAction}
+        getBookmarks={getBookmarks}
       />
     </Layout>
   );
@@ -94,9 +104,9 @@ const mapStateToProps = (state: IRootState) => ({
   currentUser: state.authReducer.currentUser,
   posts: state.postReducer,
   users: state.userReducer,
-  // bookmarks: state.bookmarkReducer,
+  bookmarks: state.bookmarkReducer,
 });
 
-export default connect(mapStateToProps, { getAllPostAction, getAllUsersAction })(
+export default connect(mapStateToProps, { getAllPostAction, getAllUsersAction, getBookmarks })(
   BookmarksConnectWithStore
 );
