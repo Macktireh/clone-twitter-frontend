a94fea88f7ba8c3b4e3cd6d121306f8953b48458
Macktireh <abdimack97@gmail.com>
2023-01-28 00:30:36 +0100

    improvement a search component for messaging

--- src/components/messages/SearchPeople.tsx
+++ src/components/messages/SearchPeople.tsx
@@ -4,14 +4,22 @@ import InputSearch from "@/widgets/InputSearch";
 import IconSVG from "@/widgets/IconSVG";
 
 const SearchPeople = () => {
+  const [isSearching, setIsSearching] = React.useState(false); // eslint-disable-line react/no-unused-state
+
   return (
     <div className="SearchPeople">
-      <label htmlFor="search-twitter">
+      <label
+        htmlFor="search-twitter"
+        onClick={() => setIsSearching(!isSearching)}
+        style={{ display: isSearching ? "none" : "flex" }}
+      >
         <IconSVG iconName="explore" fill="#919090" />
         <span>Search Direct Messages</span>
       </label>
-      <div className="searching">
-        <IconSVG iconName="back" fill="#919090" />
+      <div className="searching" style={{ display: isSearching ? "flex" : "none" }}>
+        <div className="back" onClick={() => setIsSearching(!isSearching)}>
+          <IconSVG iconName="back" fill="#919090" />
+        </div>
         <InputSearch />
       </div>
     </div>
